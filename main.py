2d6060cf9e0e1c98201c49e3f31a9a103a1e7f22
Ben <45116438+benmacmillan@users.noreply.github.com>
2020-09-28 02:08:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,19 +2,30 @@
 # Lets convert normal English into the latex commands.
 
 import pyperclip
+from tkinter import *
 
-def main ():
-    terms = {'and': '\land', 'or': '\lor', 'not': '\\neg', 'implies': '\implies','if':'','then':'\supset'}
+# tkinter setup and window config
+root = Tk()
+root.title("English2LatexLogic")
+root.geometry("300x100")
+root.resizable(width=False, height=False)
 
-    input_string = input('please type sentence: ')
-    if input_string != ('quit'):
-        print("The original string is : " + str(input_string))
-        conversion = " ".join(terms.get(word, word) for word in input_string.split())
-        print("LaTeX: " + str(conversion)) #I did have $$ each side for math mode but maybe not for now.
+input = Entry(root, width=40)
+input.pack()
+output = Text(height=3, width=30)
+output.pack()
+
+def submit_a(self):
+    terms = {'and':'\land', 'or':'\lor', 'not':'\\neg', 'implies':'\implies', 'if':'', 'then': '\supset'}
+    inputSubmit = input.get()
+    if inputSubmit != ('quit'):
+        conversion = " ".join(terms.get(word, word) for word in inputSubmit.split())
+        print(conversion)
+        output.insert(END, conversion + '\n')
         pyperclip.copy(str(conversion))
-
+        input.delete(0, 'end')
     else:
         print('exiting')
         exit()
-while True:
-    main()
+input.bind("<Return>", submit_a)
+root.mainloop()
